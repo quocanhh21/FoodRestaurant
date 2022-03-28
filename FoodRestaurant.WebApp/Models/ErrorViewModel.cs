ad714873db709afddf7e795bfb41f2f7f02cb0c9
admin <admin@DESKTOP-PENMKAK>
2022-03-28 14:35:18 +0700

    Add project files.

---
+++ FoodRestaurant.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodRestaurant.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
