95662611633f357d9bac603398a0cda8e080f39f
Le Quoc Anh <le.quocanhh21@gmail.com>
2022-03-28 21:37:41 +0700

    Add entity

---
+++ FoodRestaurant.Data/EF/FoodRestaurantDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FoodRestaurant.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRestaurant.Data.EF
+{
+    public class FoodRestaurantDbContext : DbContext
+    {
+        public FoodRestaurantDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
---
+++ FoodRestaurant.Data/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRestaurant.Data.Enums
+{
+    public enum Status
+    {
+        InActive,
+        Active
+    }
+}
