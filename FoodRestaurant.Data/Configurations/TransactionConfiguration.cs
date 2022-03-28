6561505b1336744421126c8d7b97fd809731b63e
Le Quoc Anh <le.quocanhh21@gmail.com>
2022-03-28 23:24:57 +0700

    migration

---
+++ FoodRestaurant.Data/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodRestaurant.Data.Entities;
+
+namespace FoodRestaurant.Data.Configurations
+{
+    class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+        }
+    }
+}
